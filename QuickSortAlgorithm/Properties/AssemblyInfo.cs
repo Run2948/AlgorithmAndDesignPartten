cdb05e44daa36a52277c9a56eff7bcad7b2c6b4a
少年那已不再 <15997352948@163.com>
2018-05-05 21:43:29 +0800

    添加了一个快速排序算法

--- QuickSortAlgorithm/Properties/AssemblyInfo.cs
+++ QuickSortAlgorithm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("CSharpAlgorithm")]
+[assembly: AssemblyTitle("QuickSortAlgorithm")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharpAlgorithm")]
+[assembly: AssemblyProduct("QuickSortAlgorithm")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("d5b5b7ea-7035-4fc5-bf9f-b2d551f51bc1")]
+[assembly: Guid("2aa42037-abff-4615-aeb2-cfb6e5eacdc5")]
 
 // 程序集的版本信息由下列四个值组成: 
 //
